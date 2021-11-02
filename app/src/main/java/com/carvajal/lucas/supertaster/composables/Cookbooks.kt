6a17b20620792435d87cbdb23431db6f00713c52
Lucas Carvajal <lcs.carvajal@gmail.com>
2021-11-01 22:36:39 -0700

    finish cookbooks page

--- app/src/main/java/com/carvajal/lucas/supertaster/composables/Cookbooks.kt
+++ app/src/main/java/com/carvajal/lucas/supertaster/composables/Cookbooks.kt
@@ -1,32 +1,31 @@
 package com.carvajal.lucas.supertaster.composables
 
 import android.widget.Toast
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Person
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.carvajal.lucas.supertaster.data.Cookbook
 import com.carvajal.lucas.supertaster.ui.theme.SupertasterTheme
 import com.carvajal.lucas.supertaster.viewmodels.CookbookViewModel
-import com.carvajal.lucas.supertaster.viewmodels.DashboardViewModel
 
 @Composable
 fun CookbooksScreen() {
     val viewModel = CookbookViewModel()
     val context = LocalContext.current
     val scrollState = rememberScrollState()
+    
+    val cookbooks = viewModel.getCookbooks()
 
     Box(
         modifier = Modifier
@@ -34,19 +33,41 @@ fun CookbooksScreen() {
             .padding(10.dp)
     ) {
         Column (modifier = Modifier
-            .verticalScroll(scrollState)
+            .verticalScroll(scrollState),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
             TopRow(heading = "MY COOKBOOKS", icon = Icons.Default.Add) {
-                Toast.makeText(context, "Profile", Toast.LENGTH_SHORT).show()
+                Toast.makeText(context, "Add", Toast.LENGTH_SHORT).show()
             }
-//            SuggestionsCard(viewModel.getSuggestions())
-//            SearchCard()
-//            AllRecipesCard(viewModel.getSampleRecipes())
-//            Spacer(Modifier.padding(5.dp))
+            if (cookbooks.isNotEmpty()) {
+                cookbooks.forEach { cookbook ->
+                    CookbookEntry(cookbook)
+                }
+            } else {
+                Text(
+                    text = "Tap the + to create your first cookbook",
+                    modifier = Modifier.padding(top = 30.dp)
+                )
+            }
+            Spacer(Modifier.padding(5.dp))
         }
     }
 }
 
+@Composable
+fun CookbookEntry(cookbook: Cookbook) {
+    Card(modifier = Modifier
+        .fillMaxWidth()
+        .padding(top = 20.dp),
+        elevation = mainCardElevation
+    ) {
+        Text(
+            text = cookbook.name,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(20.dp)
+        )
+    }
+}
 
 
 @Preview(showBackground = true)
--- app/src/main/java/com/carvajal/lucas/supertaster/viewmodels/CookbookViewModel.kt
+++ app/src/main/java/com/carvajal/lucas/supertaster/viewmodels/CookbookViewModel.kt
@@ -7,7 +7,20 @@ class CookbookViewModel : ViewModel() {
 
     fun getCookbooks(): List<Cookbook> {
         //TODO
-        return listOf(Cookbook(0, "Burgers"))
+//        return emptyList()
+        return listOf(
+            Cookbook(0, "Burgers"),
+            Cookbook(1, "Thai"),
+            Cookbook(2, "Soups"),
+            Cookbook(3, "BBQ"),
+            Cookbook(4, "French"),
+            Cookbook(5, "One Pot"),
+            Cookbook(6, "Mediterranean"),
+            Cookbook(7, "Steak"),
+            Cookbook(8, "Noodles"),
+            Cookbook(9, "Pastries"),
+            Cookbook(9, "Sushi")
+        )
     }
 
 }
