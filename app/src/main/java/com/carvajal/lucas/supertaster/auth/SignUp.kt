dd861c306a055c3142cd183be426e21f1a9bb80c
lucas <lcs.carvajal@gmail.com>
2021-11-06 23:33:03 -0700

    add password visibility toggle

--- app/src/main/java/com/carvajal/lucas/supertaster/auth/SignUp.kt
+++ app/src/main/java/com/carvajal/lucas/supertaster/auth/SignUp.kt
@@ -2,12 +2,15 @@ package com.carvajal.lucas.supertaster.auth
 
 import android.widget.Toast
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.carvajal.lucas.supertaster.viewmodels.ProfileViewModel
@@ -20,6 +23,8 @@ fun SignUp(navController: NavController) {
 
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
+
+    var passwordVisibility by remember { mutableStateOf(false) }
 
     Box(
         modifier = Modifier
@@ -46,9 +51,21 @@ fun SignUp(navController: NavController) {
                 onValueChange = { newPassword ->
                     password = newPassword
                 },
+                visualTransformation = if (passwordVisibility) VisualTransformation.None else PasswordVisualTransformation(),
                 singleLine = true,
                 label = {
                     Text("Password")
+                },
+                trailingIcon = {
+                    val image = if (passwordVisibility)
+                        Icons.Filled.Visibility
+                    else Icons.Filled.VisibilityOff
+
+                    IconButton(onClick = {
+                        passwordVisibility = !passwordVisibility
+                    }) {
+                        Icon(imageVector  = image, "")
+                    }
                 },
                 modifier = Modifier
                     .padding(10.dp)
